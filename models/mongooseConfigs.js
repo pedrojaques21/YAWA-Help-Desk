de448fe8f54e0b961906a9c99d4b7968c616133e
pedrojaques21 <pedronjaques1@gmail.com>
2022-05-19 20:31:04 +0100

    working chat from class

--- models/mongooseConfigs.js
+++ models/mongooseConfigs.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-const connectionString = 'mongodb+srv://DBW17:PlX4Qc8nQAsvuraM@clusterdbw.1dbjr.mongodb.net/DBW17?authSource=admin&replicaSet=atlas-bek8xj-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true';
 
-mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+mongoose.connect(process.env.CONN_STRING, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(()=>{
         console.log('MongoDB is Connected')
     });
